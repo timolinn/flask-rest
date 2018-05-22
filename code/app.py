a6ffc82a4584711628e3b5c6c49d9e021a14583b
timolinn <fabrobocomx@gmail.com>
2018-05-22 09:21:59 +0100

    Integration sql database

--- code/app.py
+++ code/app.py
@@ -1,37 +1,54 @@
 from flask import Flask, request
-from flask_restful import Resource, Api
+from flask_restful import Resource, Api, reqparse
+from flask_jwt import JWT, jwt_required
+
+import os
+
+from security import authenticate, identity
 
 app = Flask(__name__)
+app.secret_key = os.urandom(16)
 api = Api(app)
+
+jwt = JWT(app, authenticate, identity)
 
 items = []
 
 class Item(Resource):
+    parser = reqparse.RequestParser()
+    parser.add_argument('price',
+        type=float,
+        required=True,
+        help="This field is required"
+    )
+
+    @jwt_required()
     def get(self, name):
         item = next(filter(lambda x: x['name'] == name, items), None)
-        return {'item': item} 200 if item else 404
+        return {'item': item}, 200 if item else 404
 
     def post(self, name):
         if next(filter(lambda x: x['name'] == name, items), None):
             return {'message': f'Item with name {name} already exists'}, 400
 
-
+        data = Item.parser.parse_args()
         new_item = {
             'name': name,
-            'price': 0.00
+            'price': data['price']
         }
 
         items.append(new_item)
         return new_item, 201
 
     def update(self, name):
-        data = request.get_json()
+        data = Item.parser.parse_args()
         for item in items:
-            if item['item'] == name:
-                return {'update': data }
+            if item['name'] == name:
+                item.update(data)
+                return item
         new_item = {
             'name': name,
-            'price': 0.00
+            'price': data['price']
         }
 
         items.append(new_item)
@@ -39,7 +56,25 @@ class Item(Resource):
 
 
     def delete(self, name):
-        pass
+        global items
+        items = list(filter(lambda x: x['name'] != name, items))
+        return {'message': 'Item Deleted'}
+
+    def put(self, name):
+        parser = reqparse.RequestParser()
+        parser.add_argument('price',
+            type=float,
+            required=True,
+            help="This field is required"
+        )
+        data = parser.parse_args()
+        item = next(filter(lambda x: x['name'] == name, items), None)
+        if item is None:
+            item = {'name': name, 'price': data['price']}
+            items.append(data)
+        else:
+            item.update(data)
+        return item
 
     def index(self):
         return items
