c67fbf9cae8e91e04ab8debc585ea53de17adcbe
timolinn <fabrobocomx@gmail.com>
2018-05-26 09:51:22 +0100

    Fixed not importing db when running the app

--- mainproject/app.py
+++ mainproject/app.py
@@ -15,9 +15,6 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = os.urandom(16)
 api = Api(app)
 
-@app.before_first_request
-def create_tables():
-    db.create_all()
 
 jwt = JWT(app, authenticate, identity)
 
---
+++ mainproject/run.py
@@ -0,0 +1,9 @@
+import db from db
+import app from app
+
+db.init_app(app)
+
+
+@app.before_first_request
+def create_tables():
+    db.create_all()
